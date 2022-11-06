3dc01ba84fb41baa194c85e69bc075b749ff4b84
DavidUdala <david_udala@hotmail.com>
2022-11-06 20:22:35 -0300

    Create sem validacao de nada criado

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,7 +1,7 @@
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { User } from '../interface/user';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +13,12 @@ export class UserService {
 
   getAll() : Observable<User[]>{
     return this.http.get<User[]>(this.API + "/getAll")
+  }
+  create(user: User): Observable<User> {
+    return this.http.post<User>(this.API + "/create", user, {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      })
+    });
   }
 }
